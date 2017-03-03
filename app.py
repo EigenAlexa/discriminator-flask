6c16efa8eea247ba73993d97c2887bf157f69695
JamesMBartlett <jmabartlett@berkeley.edu>
2017-03-03 02:11:12 +0000

    added flask alice

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def handleRequest():
     text = request.form['text']
-    return "Hi there! You said: {}".format(text)
+    response = discriminate(text)
+    return response
 
+def discriminate(text):
+    try:
+        res = requests.post('https://3ss5b0g2q4.execute-api.us-east-1.amazonaws.com/production',{'text':text})
+        return str(res.content)
+    except requests.exceptions.RequestException:
+        return "Could you repeat that, please?"
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=80)
